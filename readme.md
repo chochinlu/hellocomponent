2639b29faf0fa5e722d248e8fc287994c727c46a
Cho-Ching Lu <chochinlu@gmail.com>
2018-09-06 17:39:05 +0800

    misc update

---
+++ readme.md
@@ -0,0 +1,4 @@
+# hello component
+
+Write the webpack4/babel7 boilerplate for test publishing react component npms.
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Hello from './components/Hello';
-
-const element = <Hello />;
-
-ReactDOM.render(element, document.getElementById("app"));
-
+export Hello from './components/Hello';
